018e3d62287dd74e15cd67ab1d1072bb6858c346
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-07-30 16:36:23 -0700

    7 solved getLastReview function, s1 and solved getReviewByRating function

--- index.js
+++ index.js
@@ -140,11 +140,10 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(array) {
+  return `${array[array.length - 1].name} gave the restaurant a ${array[array.length - 1].rating} star review, and their feedback was: ${array[array.length - 1].feedback}`;
 } 
-
-
+console.log(getLastReview(reviews));
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
 
@@ -162,10 +161,19 @@ Use the getReviewsByRating function below to do the following:
   ]
 */
 
- function getReviewByRating(/* code here */) {
-    /* code here */
+ function getReviewByRating(array, rating) {
+    const filteredArr = [];
+    for(let i = 0; i < array.length; i++) {
+      if(Math.floor(array[i].rating) === rating) {
+        filteredArr.push(array[i]);
+      } else {
+        continue;
+      }
+    }
+    return filteredArr;
   }
 
+console.log(getReviewByRating(reviews, 4));
   
 /* 💪💪💪💪💪💪💪💪💪💪 STRETCH 2: 💪💪💪💪💪💪💪💪💪💪   
 Use the getLongReviews function below to do the following:
